35966323e3575ffdf08561ada310756d3913952f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-24 13:14:34 -0700

    executing queries until get_grade_by_github_title

--- hackbright.py
+++ hackbright.py
@@ -20,16 +20,16 @@ def connect_to_db(app):
     db.init_app(app)
 
 
-def get_student_by_github(github):
+def get_student_by_github(the_github):
     """Given a GitHub account name, print info about the matching student."""
 
     QUERY = """
         SELECT first_name, last_name, github
         FROM students
-        WHERE github = :github
+        WHERE github = :gh
         """
 
-    db_cursor = db.session.execute(QUERY, {'github': github})
+    db_cursor = db.session.execute(QUERY, {'gh': the_github})
 
     row = db_cursor.fetchone()
 
@@ -42,17 +42,47 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    pass
+
+    QUERY = """
+        INSERT INTO students (first_name, last_name, github)
+          VALUES (:first_name, :last_name, :github)
+        """
+
+    db.session.execute(QUERY, {'first_name': first_name,
+                               'last_name': last_name,
+                               'github': github})
+    db.session.commit()
+
+    print(f"Successfully added student: {first_name} {last_name}")
+
+
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+    QUERY = """
+        SELECT description 
+        FROM projects 
+        WHERE title = :t
+    """
+
+    db_cursor = db.session.execute(QUERY, {'t': title})
+
+    title_description = db_cursor.fetchone()
+
+    print("The description of {} is: {}".format(title, title_description[0]))
 
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
-    pass
+    QUERY = """SELECT grade FROM grades 
+            WHERE student_github = :stud_git and project_title = :t"""
+
+    db_cursor = db.session.execute(QUERY, {'stud_git': github, 
+                                            't': title})
+    github_title = db_cursor.fetchone()
+    print('The grade for title:{} and github: {} is {}'.format(title, github, github_title[0]))
+
 
 
 def assign_grade(github, title, grade):
@@ -91,7 +121,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    # handle_input()
+    #handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
